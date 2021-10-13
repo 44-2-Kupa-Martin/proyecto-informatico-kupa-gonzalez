3d9ef7a49d96f4d6fb0cea6edb4888d7d488dd64
joacozimer <joacozimerman@gmail.com>
2021-10-13 17:32:26 -0300

    schemas

--- api/models/Note.js
+++ api/models/Note.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose');
 
 const NoteSchema = new mongoose.Schema({
-  id: {type: , required: true },
+  id: {type: Int, required: true },
   title: { type: String, required: true },
   text: { type: String, required: true },
   createdAt: { type: Date, default: Date.now },
   updatedAt: { type: Date, default: Date.now },
-  userdata:{
-    src: {type: img, required: true},
-    username: {type: string, required: true}
-  }
+
 });
 
+const UserSchema = new mongoose.Schema({
+  avatar: {type: Img, required: true},
+  username:{type: String, unique: true, required: true},
+  logindate: Date
+});
+
+const User = moongoose.model('User'),UserSchema;
 const Note = mongoose.model('Note', NoteSchema);
 
 module.exports = Note;
