ce8bad7874237b915337c705dacb005050f6b99e
Winterishtuba <winterishtuba@gmail.com>
2021-10-27 15:10:04 -0300

    multer

---
+++ api/routes/messages.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const Note = require('../models/Note');
+
+app.get('/messages', (req, res, next) => {
+  
+ 
+  
+  
+})
+
+module.exports = router;
